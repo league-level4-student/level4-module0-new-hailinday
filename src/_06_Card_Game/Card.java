8e659e44fe16a869bb4a5c617c8ee9a11b8df369
league <league@imac-55.attlocal.net>
2022-07-29 17:01:22 -0700

    stuff

--- src/_06_Card_Game/Card.java
+++ src/_06_Card_Game/Card.java
@@ -21,15 +21,15 @@ public class Card {
     public String toString() {
         return rank + " of " + suit;
     }
-    public enum suit {
+    public enum Suit {
     	Spades, Clubs, Hearts, Diamonds;
     }
-    public enum rank {
+    public enum Rank {
     	Two(2), Three(3), Four(4), Five(5), Six(6), Seven(7), Eight(8), Nine(9), Ten(10), Jack(11), Queen(12), King(13), Ace(14);
     	
     	private final int value;
     	
-    	rank(int value) {
+    	Rank(int value) {
 			this.value = value;
 		}
 
--- src/_06_Card_Game/CardDealer.java
+++ src/_06_Card_Game/CardDealer.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 
+import _06_Card_Game.Card.Rank;
+import _06_Card_Game.Card.Suit;
+
 public class CardDealer {
     public static void main(String[] args) {
-    	ArrayList<Card> deck;
-        for (int i = 0; i < args.length; i++) {
-			String string = args[i];
-			
-		}
+        
 	}
+    public void deckMake (Rank rank, Suit suit) {
+    	ArrayList<Card> deck = new ArrayList<Card>();
+    	for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 13; j++) {
+				deck.add();
+			}
+		}
+    }
 }
